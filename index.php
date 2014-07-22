48dbd6d45ec2c4b2ff31f603f6d2e419584b4844
danielheth <danielheth@hotmail.com>
2014-07-22 17:38:33 -0500

    updated api

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+$app->get('/', function () {
+	echo "Minion v1.0";
+});
+
 $app->get('/hello/:name', function ($name) {
 	echo "Hello, $name";
 });
+
+
 
 $app->run();
