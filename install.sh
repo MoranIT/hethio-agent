dc522b54b0ebb7f06dd4b2841ef60fb77a8711e5
danielheth <danielheth@hotmail.com>
2014-07-22 22:50:18 -0500

    fixed installer

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ cp -f lighttpd.conf /etc/lighttpd/
 echo "Copying minion-api website into place"
 cp -f index.php /var/www/
 cp -f index.html /var/www/
-cp -f features.html /var/www/
+cp -f api.html /var/www/
 cp -f contact.html /var/www/
 rm -rf /var/www/images/
 cp -rf images/ /var/www/
