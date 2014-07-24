b5ac118567c90946e9a7f19e9185b5b01a48fd13
danielheth <danielheth@hotmail.com>
2014-07-24 10:56:58 -0500

    created common outputresponse function

--- api/index.php
+++ api/index.php
@@ -24,17 +24,17 @@ function OutputResponse($response, $singular, $multiple = null, $format = 'html'
 		$app->response->headers->set('Content Type', 'application/json');
 		echo json_encode($response);
 	} else if ($format == "xml") {
-		/*
+		
 		$app->response->headers->set('Content Type', 'text/xml');
 		echo '<?xml version="1.0" encoding="UTF-8"?>';
-		if ($isArray && $multiple != null) { echo "<".$multiple.">"; }	
+		if ($isArray && !is_null($multiple)) { echo "<".$multiple.">"; }	
 		foreach($response in $responses) {
 			echo "<".$singular.">\n";
 			//while (list($key, $val) = each($response)) { echo "<$key>$val</$key>\n"; }
 			echo "</".$singular.">\n";
 		}
-		if ($isArray && $multiple != null) { echo "</".$multiple.">\n"; }
-		*/
+		if ($isArray && !is_null($multiple)) { echo "</".$multiple.">\n"; }
+		
 	} else {
 		print_r($response);
 	}
