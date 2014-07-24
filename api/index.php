6784efe957732c233b7a23c1dad5d4fa00c5147e
danielheth <danielheth@hotmail.com>
2014-07-24 10:54:42 -0500

    created common outputresponse function

--- api/index.php
+++ api/index.php
@@ -24,6 +24,7 @@ function OutputResponse($response, $singular, $multiple = null, $format = 'html'
 		$app->response->headers->set('Content Type', 'application/json');
 		echo json_encode($response);
 	} else if ($format == "xml") {
+		/*
 		$app->response->headers->set('Content Type', 'text/xml');
 		echo '<?xml version="1.0" encoding="UTF-8"?>';
 		if ($isArray && $multiple != null) { echo "<".$multiple.">"; }	
@@ -33,6 +34,7 @@ function OutputResponse($response, $singular, $multiple = null, $format = 'html'
 			echo "</".$singular.">\n";
 		}
 		if ($isArray && $multiple != null) { echo "</".$multiple.">\n"; }
+		*/
 	} else {
 		print_r($response);
 	}
