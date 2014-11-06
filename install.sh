af7cf0f3dddf9f1ec738c8f607ffd39bbcbe8b9e
Daniel Moran <danielheth@hotmail.com>
2014-11-06 09:30:41 -0600

    clear pi specific code

--- install.sh
+++ install.sh
@@ -55,17 +55,19 @@ fi
 echo "* Updating Repository"
 apt-get update
 
-#http://brandonb.io/creating-your-own-minimalistic-rasbian-image-for-the-raspberry-pi
-echo "* Remove Misc Packages and Development"
-rm -rf python_games
-apt-get remove x11-common midori lxde lxde-common lxde-icon-theme omxplayer raspi-config -y
-apt-get remove `sudo dpkg --get-selections | grep "\-dev" | sed s/install//` -y
-#apt-get remove `sudo dpkg --get-selections | grep -v "deinstall" | grep python | sed s/install//` -y
-apt-get remove `sudo dpkg --get-selections | grep -v "deinstall" | grep x11 | sed s/install//` -y
-apt-get remove gcc-4.4-base:armhf gcc-4.5-base:armhf gcc-4.6-base:armhf -y
-apt-get remove libraspberrypi-doc xkb-data fonts-freefont-ttf -y
-apt-get autoremove -y
-apt-get clean
+
+#REMOVE MISC UNNEEDED PACKAGES FROM RASPBERRY PI'S
+##http://brandonb.io/creating-your-own-minimalistic-rasbian-image-for-the-raspberry-pi
+#echo "* Remove Misc Packages and Development"
+#rm -rf python_games
+#apt-get remove x11-common midori lxde lxde-common lxde-icon-theme omxplayer raspi-config -y
+#apt-get remove `sudo dpkg --get-selections | grep "\-dev" | sed s/install//` -y
+##apt-get remove `sudo dpkg --get-selections | grep -v "deinstall" | grep python | sed s/install//` -y
+#apt-get remove `sudo dpkg --get-selections | grep -v "deinstall" | grep x11 | sed s/install//` -y
+#apt-get remove gcc-4.4-base:armhf gcc-4.5-base:armhf gcc-4.6-base:armhf -y
+#apt-get remove libraspberrypi-doc xkb-data fonts-freefont-ttf -y
+#apt-get autoremove -y
+#apt-get clean
 
 
 
@@ -169,6 +171,15 @@ chmod 600 $MPATH/key
 
 echo "* Starting Cron"
 service cron restart
+
+
+
+
+if [ -f $MPATH/bin/register ]; then
+	cd $MPATH/bin
+	echo "Register system"
+	$MPATH/bin/register
+fi
 
 
 echo "Installation Complete... Enjoy your Minion!"
