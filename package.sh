891b146757bdc18aeb7c2e936d6632067dce8c4d
Daniel Moran <danielheth@hotmail.com>
2015-04-10 00:32:20 -0500

    adding dynamic changelog

--- package.sh
+++ package.sh
@@ -4,6 +4,22 @@ python setup.py \
 --command-packages=stdeb.command debianize \
 --suite `lsb_release -sc`
 
+
+VERSION=`git tag | tail -n 1`
+CHANGES=`git shortlog $VERSION..HEAD`
+
+TOPLINE=`head -n 1 debian/changelog`
+BOTTOMLINE=`tail -n 1 debian/changelog`
+
+echo $TOPLINE > debian/changelog
+echo "" >> debian/changelog
+for CHANGE in ${CHANGES//\\n/ } ; do
+    echo "  * $CHANGE" >> debian/changelog
+done
+echo "" >> debian/changelog
+echo $BOTTOMLINE >> debian/changelog
+
+
 python setup.py sdist
 mv dist/hethio-agent* ./
 
--- publish.sh
+++ publish.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-VERSION=`cat VERSION.txt`
 
-CHANGES=`git shortlog $VERSION..HEAD`
-echo $CHANGES > ppa4_source.changes
 
 
 dput ppa:danielheth/hethio-agent ./ppa4_source.changes
