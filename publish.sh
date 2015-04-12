5ab8bb5776fabc0a141d6fdf2239783eaa10c64a
Daniel Moran <danielheth@hotmail.com>
2015-04-12 13:57:15 -0500

    changing publish repo to hethio instead of hethio-agent

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 VERSION=`cat VERSION.txt`
 
 
-dput -f ppa:danielheth/hethio-agent ../hethio-agent_$VERSION-1_source.changes
+dput -f ppa:danielheth/hethio ../hethio-agent_$VERSION-1_source.changes
 
 
 # ===================================
