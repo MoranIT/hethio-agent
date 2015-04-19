fa998f37d73c675ae0bb4a161c56ef89b589c067
Daniel Moran <danielheth@hotmail.com>
2015-04-19 10:24:56 -0500

    adding version reporting with daemon status

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
 	py_modules = ['hethio-agent'],
 	scripts=glob.glob('bin/*'),
 	data_files = [
+		('share/hethio-agent', glob.glob('VERSION.txt')),
 		('share/hethio-agent', glob.glob('data/*')),
 		('/usr/share/man/man8', glob.glob('man/*.gz'))
 		],
